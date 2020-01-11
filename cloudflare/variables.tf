b5e587794c2f14f7072a4789c3349971b5aecdbb
Keith Hand <keith@hand.technology>
2020-01-11 03:36:36 -0600

    Cleaned up variables.tf descriptions

--- cloudflare/variables.tf
+++ cloudflare/variables.tf
@@ -1,3 +1,23 @@
-variable "token" {}
-variable "main_server_ip" {}
-variable "non_proxied_route" {}
+/**
+  These variable are private and should never be committed
+*/
+
+variable "token" {
+  description = "Token to access linode api"
+  type = string
+}
+
+// TODO: setup vpn to not expose this route public at all
+variable "non_proxied_route" {
+  description = "private route allow direct access to instance ip"
+  type = string
+}
+
+/**
+  These variables are public
+*/
+
+variable "main_server_ip" {
+  description = "IP address to point A record to"
+  type = string
+}
--- linode/variables.tf
+++ linode/variables.tf
@@ -1,4 +1,25 @@
-variable "token" {}
-variable "instance_type" {}
-variable "region" {}
-variable "non_proxied_route" {}
+/**
+  These variable are private and should never be committed
+*/
+
+variable "token" {
+  description = "Token to access linode api"
+  type = string
+}
+
+
+/**
+  These variables are public
+*/
+
+variable "instance_type" {
+  description = "The instance size to set the server as"
+  type = string
+  default = "g6-standard-1"
+}
+
+variable "region" {
+  description = "Region to place the instance"
+  type = string
+  default = "us-central-1"
+}
