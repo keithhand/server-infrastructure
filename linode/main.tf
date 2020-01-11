9a39609941d7071cf06a1b5c6b384cb7f20a0e06
Keith Hand <keith@hand.technology>
2020-01-11 06:01:55 -0600

    add resource to initialize docker-machine & updated readme

--- linode/main.tf
+++ linode/main.tf
@@ -9,3 +9,24 @@ resource "linode_instance" "server" {
   type = var.instance_type
   authorized_users = var.authorized_users
 }
+
+resource "null_resource" "docker-machine" {
+  triggers = {
+    server_ip = linode_instance.server.ip_address
+    server_name = var.server_name
+  }
+
+  provisioner "local-exec" {
+    command = "yes | docker-machine rm ${self.triggers.server_name}"
+    on_failure = continue
+  }
+
+  provisioner "local-exec" {
+    environment = {
+      GENERIC_IP_ADDRESS = self.triggers.server_ip
+      GENERIC_SSH_USER = "root"
+    }
+
+    command = "docker-machine create -d generic ${self.triggers.server_name}"
+  }
+}
